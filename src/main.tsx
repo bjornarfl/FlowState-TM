af3b0d733a4ac27e016a1f8680de4c15a2b893dd
Bjørnar Fidje Liberg <40608747+bjornarfl@users.noreply.github.com>
2026-02-13 13:29:06 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/FlowState-TM">
       <App />
     </BrowserRouter>
   </StrictMode>,
